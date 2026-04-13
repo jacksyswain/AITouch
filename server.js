a31f6da7f7d793b091ec85d97b6a636d1421373f
jyotiprakash swain <jp.swainabc@gmail.com>
2026-04-13 22:06:35 +0530

    initial setup server

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
 
 const connectDB = require("./config/db");
 const deviceRoutes = require("./routes/deviceRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
+const server = http.createServer(app);
+
+// 🔥 Socket.IO setup
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// Make io globally accessible
+app.set("io", io);
 
 // DB
 connectDB();
@@ -21,8 +34,17 @@ app.use("/api/device", deviceRoutes);
 // Error Middleware
 app.use(errorHandler);
 
+// Socket connection
+io.on("connection", (socket) => {
+  console.log("Client connected 🔌");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected ❌");
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+server.listen(PORT, () =>
   console.log(`Server running on port ${PORT} 🚀`)
 );
